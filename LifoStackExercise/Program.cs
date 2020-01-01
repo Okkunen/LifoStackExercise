4745a240593c8d1ae4677025c85be42e67364b18
Okkunen <tsemi@hotmail.com>
2020-01-01 21:14:52 +0200

    Add project files.

---
+++ LifoStackExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifoStackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code for testing
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Console.ReadLine();
+            //stack.Clear();
+            Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LifoStackExercise/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LifoStackExercise
+{
+    public class Stack
+    {
+        private List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Cannot add empty values");
+            }
+            else
+            {
+                _list.Add(obj);
+            }
+        }
+
+        public object Pop()
+        {
+            object removedItem;
+
+            if (_list.Count > 0)
+            {
+                removedItem = _list[_list.Count - 1];
+                _list.RemoveAt(_list.Count - 1);
+            }
+            else
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+            return removedItem;
+        }
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
